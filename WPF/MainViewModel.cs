045a2889ba174d66ce3406babc60b00f13814206
Tomas Heky Hekrla <tomas@heky.eu>
2025-01-02 12:25:06 +0100

    POC

--- WPF/MainViewModel.cs
+++ WPF/MainViewModel.cs
@@ -1,44 +1,104 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HekyLab.PingTray.WPF.Properties;
 using System.Collections.Concurrent;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Net.NetworkInformation;
 
 namespace HekyLab.PingTray.WPF;
 
-public class HostPingResult : INotifyPropertyChanged
+public class HostPingResult : ObservableObject
 {
-  private readonly ConcurrentQueue<IPStatus> _pingHistory = new();
-  private DateTime? _lastPingAt;
+  public const string Idle = "Idle";
 
   public required string Host { get; init; } = string.Empty;
+
+  private DateTime? _lastPingAt = null;
   public DateTime? LastPingAt => _lastPingAt;
+
+  private readonly ConcurrentQueue<IPStatus> _pingHistory = new();
   public string Status => _pingHistory.IsEmpty
-      ? "Idle"
+      ? Idle
       : _pingHistory.Take(3).GroupBy(s => s).OrderByDescending(g => g.Count()).First().Key.ToString();
 
   public void AddPingResult(IPStatus status)
   {
+    OnPropertyChanging(nameof(Status));
     _pingHistory.Enqueue(status);
     if (_pingHistory.Count > 3) _pingHistory.TryDequeue(out _);
-    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Status)));
-    _lastPingAt = DateTime.Now;
-    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastPingAt)));
+    OnPropertyChanged(nameof(Status));
+    SetProperty(ref _lastPingAt, DateTime.Now, nameof(LastPingAt));
   }
-  public event PropertyChangedEventHandler? PropertyChanged;
 }
 
-public class MainViewModel(IEnumerable<string> initialHosts) : INotifyPropertyChanged
+public partial class MainViewModel : ObservableObject
 {
-  private readonly ObservableCollection<HostPingResult> _pingResults = new(initialHosts.Select(host => new HostPingResult { Host = host }));
-  public ObservableCollection<HostPingResult> PingResults => _pingResults;
-
-  private string _newHost = string.Empty;
-
-  public string NewHost
+  private readonly Settings settings;
+  public MainViewModel(Settings settings)
   {
-    get => _newHost;
-    set { _newHost = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NewHost))); }
+    this.settings = settings;
+    pingResults = new(settings.Hosts.Cast<string>().Select(host => new HostPingResult { Host = host }));
+    pingResults.CollectionChanged += PingResults_CollectionChanged;
   }
 
-  public event PropertyChangedEventHandler? PropertyChanged;
+  private readonly ObservableCollection<HostPingResult> pingResults;
+  public ObservableCollection<HostPingResult> PingResults => pingResults;
+
+  public bool IsEveryHostHealthy => PingResults.All(p => p.Status == HostPingResult.Idle || p.Status == IPStatus.Success.ToString());
+
+  [ObservableProperty]
+  [NotifyCanExecuteChangedFor(nameof(AddHostCommand))]
+  private string? newHost;
+
+  [ObservableProperty]
+  [NotifyCanExecuteChangedFor(nameof(RemoveHostCommand))]
+  private HostPingResult? selectedPingResult;
+
+  [RelayCommand(CanExecute = nameof(CanAddHost))]
+  private void AddHost(string? host)
+  {
+    if (host is null) return;
+    PingResults.Add(new HostPingResult { Host = host });
+    NewHost = string.Empty;
+  }
+  private static bool CanAddHost(string? host) => !string.IsNullOrWhiteSpace(host);
+
+  [RelayCommand(CanExecute = nameof(CanRemoveHost))]
+  private void RemoveHost(HostPingResult? pingResult)
+  {
+    if (pingResult is null) return;
+    PingResults.Remove(pingResult);
+  }
+  private static bool CanRemoveHost(HostPingResult? pingResult) => pingResult is not null;
+
+  public async Task PingAsync()
+  {
+    var tasks = PingResults.Select(static async result =>
+    {
+      using var ping = new Ping();
+      var reply = await ping.SendPingAsync(result.Host);
+      result.AddPingResult(reply.Status);
+    });
+    await Task.WhenAll(tasks);
+  }
+
+  private void PingResults_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+  {
+    switch (e.Action)
+    {
+      case System.Collections.Specialized.NotifyCollectionChangedAction.Add:
+        settings.Hosts.AddRange(e.NewItems?.Cast<HostPingResult>().Select(s => s.Host).ToArray() ?? []);
+        settings.Save();
+        break;
+      case System.Collections.Specialized.NotifyCollectionChangedAction.Remove:
+        foreach (var item in e.OldItems?.Cast<HostPingResult>().Select(s => s.Host) ?? [])
+        {
+          settings.Hosts.Remove(item);
+        }
+        settings.Save();
+        break;
+      default:
+        throw new NotImplementedException($"Unsupported action {e.Action}");
+    }
+  }
 }
--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -2,7 +2,6 @@ using HekyLab.PingTray.WPF.Properties;
 using System.ComponentModel;
 using System.Net.NetworkInformation;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace HekyLab.PingTray.WPF;
 
@@ -12,92 +11,51 @@ namespace HekyLab.PingTray.WPF;
 public partial class MainWindow : Window
 {
   private readonly Settings settings = Settings.Default;
-  private readonly MainViewModel _viewModel = new(Settings.Default.Hosts.Cast<string>());
-  public MainViewModel ViewModel => _viewModel;
 
-  private readonly BackgroundWorker _pingWorker = new();
+  private readonly MainViewModel viewModel = new (Settings.Default);
+  public MainViewModel ViewModel => viewModel;
+
+  private readonly BackgroundWorker pingWorker = new();
+
+  private readonly NotifyIcon notifyIcon = new()
+  {
+    Icon = SystemIcons.Question,
+    Visible = true,
+    Text = "Ping Tray",
+  };
 
   public MainWindow()
   {
     InitializeComponent();
 
-    _pingWorker.DoWork += pingWorker_DoWork;
-    _pingWorker.RunWorkerAsync();
-
-    ViewModel.PingResults.CollectionChanged += PingResults_CollectionChanged;
+    pingWorker.DoWork += PingWorker_DoWork;
+    pingWorker.RunWorkerAsync();
   }
 
-  private void PingResults_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+  protected override void OnClosing(CancelEventArgs e)
   {
-    switch (e.Action)
-    {
-      case System.Collections.Specialized.NotifyCollectionChangedAction.Add:
-        settings.Hosts.AddRange(e.NewItems.Cast<HostPingResult>().Select(s => s.Host).ToArray());
-        settings.Save();
-        break;
-      case System.Collections.Specialized.NotifyCollectionChangedAction.Remove:
-        foreach (var item in e.OldItems.Cast<HostPingResult>().Select(s => s.Host))
-        {
-          settings.Hosts.Remove(item);
-        }
-        settings.Save();
-        break;
-      default:
-        throw new NotImplementedException($"Unsupported action {e.Action}");
-    }
+    base.OnClosing(e);
+    notifyIcon.Visible = false; // hack for "tray icon not disappearing" bug
   }
 
-  private async void pingWorker_DoWork(object? sender, DoWorkEventArgs e)
+  private void UpdateTrayIcon()
+  {
+    notifyIcon.Icon = ViewModel.IsEveryHostHealthy switch
+    {
+      true => SystemIcons.Information,
+      false => SystemIcons.Error,
+    };
+  }
+
+  private async void PingWorker_DoWork(object? sender, DoWorkEventArgs e)
   {
     if (sender is not BackgroundWorker worker) return;
 
     while (!worker.CancellationPending)
     {
-      var tasks = _viewModel.PingResults.Select(static async result =>
-      {
-        using var ping = new Ping();
-        var reply = await ping.SendPingAsync(result.Host);
-        result.AddPingResult(reply.Status);
-      });
-      await Task.WhenAll(tasks);
-      await Task.Delay(Settings.Default.DelayBetweenPings);
-    }
-  }
-
-  private void command_addHost_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
-  {
-    e.CanExecute = !string.IsNullOrWhiteSpace(_viewModel.NewHost) && !Validation.GetHasError(textBox_newHost);
-  }
-
-  private void command_addHost_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
-  {
-    _viewModel.PingResults.Add(new HostPingResult { Host = _viewModel.NewHost });
-    _viewModel.NewHost = string.Empty;
-    textBox_newHostNote.Text = string.Empty;
-  }
-
-  private void command_deleteHost_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
-  {
-    e.CanExecute = listView_pingResults.SelectionMode == SelectionMode.Single
-      ? listView_pingResults.SelectedItem is not null
-      : listView_pingResults.SelectedItems.Count > 0;
-  }
-
-  private void command_deleteHost_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
-  {
-    if (e.Source is ListView listView)
-    {
-      if (listView.SelectionMode == SelectionMode.Single && listView.SelectedItem is HostPingResult result)
-      {
-        _viewModel.PingResults.Remove(result);
-      }
-      else
-      {
-        foreach (var item in listView.SelectedItems.Cast<HostPingResult>())
-        {
-          _viewModel.PingResults.Remove(item);
-        }
-      }
+      await ViewModel.PingAsync();
+      UpdateTrayIcon();
+      await Task.Delay(settings.DelayBetweenPings);
     }
   }
 }
